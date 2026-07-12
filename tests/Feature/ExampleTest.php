6500144f27f7effb72d985bcb9657c32b3e80ca9
nagaki <nagaki.0813@gmail.com>
2026-07-12 15:31:17 +0900

    Complete basic requerements checks

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,18 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    use RefreshDatabase;
+
+    public function test_books_index_returns_successful_response(): void
     {
-        $response = $this->get('/');
+        $this->seed();
+
+        $response = $this->get('/books');
 
         $response->assertStatus(200);
     }
